3fcf5f5c31ec6f0fccdfd3f21ef729f323387e55
amandaeloah <amandaeloa28@gmail.com>
2026-05-14 09:34:23 -0300

    cadastro de cliente

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
  
  
  
-from .forms import ClienteForm
+from .forms import ClienteForm, UsuarioForm
 from .models import Cliente
  
  
@@ -78,9 +78,26 @@ def login_usuario(request):
             else:
                 return HttpResponse('<h1>Usuário ou senha inválidos<h1>')
     else:
-            form = AuthenticationForm()
-            context = {
-                'form': form
-            }
+        form = AuthenticationForm()
+    context = {
+       'form': form
+    }
     return render(request, template_name, context)
+
+def novo_usuario(request):
+    template_name = 'novo_usuario.html'
+    if request.method =='POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+          f = form.save(commit=False)
+          f.set_password(f.password)
+          f.save()
+          return redirect('login_usuario')
+        else:
+          return HttpResponse('Erro ao criar usuário')
+    else:
+        form = UsuarioForm()
+    context = {'form': form}
+    return render(request, template_name, context)
+ 
  
