7e17f9cc1c8d14110e384aefb5f494c585870279
amandaeloah <amandaeloa28@gmail.com>
2026-05-15 09:21:04 -0300

    modificacion

--- cliente/urls.py
+++ cliente/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from cliente import views 
+from clientes import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
